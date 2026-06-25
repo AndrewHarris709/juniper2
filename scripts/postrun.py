85b1beceb32a12b4369b9757919c6faf5cdbfc30
Andrew Harris <arharris@mun.ca>
2026-06-25 00:14:32 -0230

    Python postscript test

---
+++ scripts/postrun.py
@@ -0,0 +1,12 @@
+import sarracen as sar
+import matplotlib.pyplot as plt
+
+print("Hello World!")
+
+sdf = sar.read_csv("test.csv")
+sdf.params['hfact'] = 1.4
+sdf.params['mass'] = 3.0517578125e-05;
+sdf.calc_density()
+sdf.render(target="rho")
+
+plt.show()
